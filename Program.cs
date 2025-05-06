488b89a4e937eacfa171c553937c8ada3b415908
YandaNet <meaksymdyndyn@gmail.com>
2025-05-06 17:07:11 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace ConsoleApp11
+﻿namespace dz
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int width;
+            char symbol;
+            Console.WriteLine("Enter dov");
+            width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter symbol");
+            symbol = char.Parse(Console.ReadLine());
+            Kvadrat(width, symbol);
+        }
+
+        static void Kvadrat(int dovzyna, char symbol)
+        {
+            for (int i = 0; i < dovzyna; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < dovzyna; j++)
+                {
+                    Console.Write(symbol);
+                }
+            }
         }
     }
 }
